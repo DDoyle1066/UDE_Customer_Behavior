28c04a61328604ab5c5df82224e5bd7865e4250a
Daniel Doyle <dpddoyle.doyle@gmail.com>
2023-04-30 08:17:40 -0400

    refixed spelling

--- Julia/Weave.jl
+++ Julia/Weave.jl
@@ -2,4 +2,4 @@ using Pkg: Pkg
 Pkg.activate(".")
 using Weave
 weave("Introduction.jmd"; out_path="html/", fig_path=tempname())
-weave("Latka-Volterra.jmd"; out_path="html/", fig_path=tempname())
+weave("Lotka-Volterra.jmd"; out_path="html/", fig_path=tempname())
