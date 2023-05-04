8a1bd320af8144d5a44f98a6c95aa1f81992f96d
Daniel Doyle <dpddoyle.doyle@gmail.com>
2023-05-04 04:14:40 -0400

    Added SIR file

--- Julia/Weave.jl
+++ Julia/Weave.jl
@@ -3,3 +3,4 @@ Pkg.activate(".")
 using Weave
 weave("Introduction.jmd"; out_path="html/", fig_path=tempname())
 weave("Lotka-Volterra.jmd"; out_path="html/", fig_path=tempname())
+weave("SIR.jmd"; out_path="html/", fig_path=tempname())
